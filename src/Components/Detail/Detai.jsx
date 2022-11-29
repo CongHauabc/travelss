b7d375de68c4955b0faf6a3acafe13795d5662dd
nguyễn Hậu <nguyenconghau12t2nh2019@gmail.com>
2022-11-29 19:21:25 +0700

    done detail

--- src/Components/Detail/Detai.jsx
+++ src/Components/Detail/Detai.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import './detail.css';
-import { Data } from '../Database/db';
-import { useParams } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import "./detail.css";
+import { Data } from "../Database/db";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { Link } from "react-router-dom";
 const Detai = () => {
-    const [detail,setDetail] = useState({})
-    useEffect(()=>{
-        const details= Data.find((datas)=>{
-            return datas.id = param.id
-        })
-        
-        setDetail(details)
-        
-    },[])
-    const param = useParams()
-    console.log(detail);
-    return (
-        
-        <div className='detail'>
-             <div className='detailContent'>
-           <h1>{detail.destTitle}</h1>
-           
-             
-             </div>
+  const [detail, setDetail] = useState({});
+  useEffect(() => {
+    const details = Data.find((datas) => {
+      return (datas.id = param.id);
+    });
+
+    setDetail(details);
+  }, []);
+ 
+  const param = useParams();
+  console.log(detail);
+  return (
+    <div className="detail">
+      <div className="detailContent">
+        <div className="imgdetail">
+          <img src={detail.imgSrs} />
         </div>
-    );
-}
+        <div className="detailscontent">
+          <h1>{detail.destTitle}</h1>
+          <p>{detail.location}</p>
+          <p>${detail.fees}</p>
+
+          <p>{detail.description}</p>
+          <button className="btn">Liên Hệ</button>
+          <Link to={'/'} className='btnclose'>
+            <AiOutlineCloseCircle className="icon" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Detai;
